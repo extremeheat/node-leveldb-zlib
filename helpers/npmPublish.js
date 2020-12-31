7c306f06cc201db0bc4dfce7353d14ad04fb45f3
extremeheat <extreme@protonmail.ch>
2020-12-31 18:57:29 -0500

    add npmPublish.js

---
+++ helpers/npmPublish.js
@@ -0,0 +1,11 @@
+const fs = require('fs')
+
+const proc = require('child_process')
+
+console.log('Current dir:')
+
+if (process.platform == 'win32') {
+    proc.execSync('dir /s /b')
+} else {
+    proc.execSync('ls -r')
+}
