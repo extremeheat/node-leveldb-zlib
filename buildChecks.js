3d6f958d4aa9684e3e21afb0bb52d982a29e1f44
extremeheat <extreme@protonmail.ch>
2020-12-31 02:29:24 -0500

    linux fixes

---
+++ buildChecks.js
@@ -0,0 +1,31 @@
+// Some helpful pre-build enviornment checks
+const fs = require('fs')
+
+if (!fs.existsSync('./leveldb-mcpe/include')) {
+  console.error('******************* READ ME ****************\n')
+  console.error(' You did not clone recursively. You need to run git submodule init && git submodule update\n')
+  console.error('******************* READ ME ****************\n')
+  process.exit(1)
+}
+
+if (process.platform == 'win32') {
+  if (!process.env.CMAKE_TOOLCHAIN_FILE) {
+    console.error('******************* READ ME ****************\n')
+    console.error(' CMAKE_TOOLCHAIN_FILE was not set. Please see the Windows build steps at https://github.com/extremeheat/node-leveldb-zlib/\n')
+    console.error(' The build below probably failed.\n')
+    console.error('******************* READ ME ****************\n')
+  }
+} else if (process.platform == 'darwin') {
+  if (!fs.existsSync(`/usr/local/opt/zlib/include/`)) {
+    console.error('******************* READ ME ****************\n')
+    console.error(' zlib was not found. Run `xcode-select --install` and try again.\n')
+    console.error(' The build below probably failed.\n')
+    console.error('******************* READ ME ****************\n')
+  }
+} else {
+  if (!fs.existsSync(`/usr/include/zlib.h`) && !fs.existsSync(`/usr/local/include/zlib.h`)) {
+    console.error('******************* READ ME ****************\n')
+    console.error(' zlib headers were not found. If the build fails, try `sudo apt-get install libz-dev`\n')
+    console.error('******************* READ ME ****************\n')
+  }
+}
