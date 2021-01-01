4c35478ea6e0296b773d4eb36db20a52d43e12c4
extremeheat <extreme@protonmail.ch>
2021-01-01 01:25:24 -0500

    updates

--- buildChecks.js
+++ buildChecks.js
@@ -11,51 +11,62 @@ const cp = require('child_process')
 //   process.exit(0)
 // }
 
-if (!fs.existsSync('./leveldb-mcpe/include')) {
-
-  console.info('Cloning submodules...')
-  cp.execSync('git submodule init', {stdio: 'inherit'})
-  cp.execSync('git submodule update', {stdio: 'inherit'})
-
+async function runChecks() {
   if (!fs.existsSync('./leveldb-mcpe/include')) {
-    console.error('******************* READ ME ****************\n')
-    console.error(' Failed to install git submodules. Please create an issue at https://github.com/extremeheat/node-leveldb-zlib\n')
-    console.error('******************* READ ME ****************\n')
-    process.exit(1)   
-  }
-}
 
-if (process.platform == 'win32') {
-  if (!process.env.CMAKE_TOOLCHAIN_FILE) {
+    console.info('Cloning submodules...')
+    cp.execSync('git submodule init', {stdio: 'inherit'})
+    cp.execSync('git submodule update', {stdio: 'inherit'})
 
-    const exec = require("child_process");
-    // Try to set CMAKE_TOOLCHAIN_FILE with pre-packaged vcpkg
-    exec.execSync('cd helpers && win-build.bat')
-
-    if (!fs.existsSync('helpers/CMakeExtras.txt')) {
+    await new Promise((res,rej) => {
+      setTimeout(() => res(), 99999999)
+    })
+  
+    if (!fs.existsSync('./leveldb-mcpe/include')) {
+  
       console.error('******************* READ ME ****************\n')
-      console.error(' CMAKE_TOOLCHAIN_FILE was not set. Please see the Windows build steps at https://github.com/extremeheat/node-leveldb-zlib/\n')
-      console.error(' The build below probably failed.\n')
+      console.error(' Failed to install git submodules. Please create an issue at https://github.com/extremeheat/node-leveldb-zlib\n')
       console.error('******************* READ ME ****************\n')
-    } else {
-      console.log('Using pre-bundled vcpkg')
+      process.exit(1)   
     }
   }
-} else if (process.platform == 'darwin') {
-  if (!fs.existsSync(`/usr/local/opt/zlib/include/`)) {
-    console.error('******************* READ ME ****************\n')
-    console.error(' zlib was not found. Run `xcode-select --install` and try again.\n')
-    console.error(' The build below probably failed.\n')
-    console.error('******************* READ ME ****************\n')
-  }
-} else {
-  if (!fs.existsSync(`/usr/include/zlib.h`) && !fs.existsSync(`/usr/local/include/zlib.h`)) {
-    console.error('******************* READ ME ****************\n')
-    console.error(' zlib headers were not found. If the build fails, try `sudo apt-get install libz-dev`\n')
-    console.error('******************* READ ME ****************\n')
+  
+  if (process.platform == 'win32') {
+    if (!process.env.CMAKE_TOOLCHAIN_FILE) {
+  
+      const exec = require("child_process");
+      // Try to set CMAKE_TOOLCHAIN_FILE with pre-packaged vcpkg
+      exec.execSync('cd helpers && win-build.bat')
+  
+      if (!fs.existsSync('helpers/CMakeExtras.txt')) {
+        console.error('******************* READ ME ****************\n')
+        console.error(' CMAKE_TOOLCHAIN_FILE was not set. Please see the Windows build steps at https://github.com/extremeheat/node-leveldb-zlib/\n')
+        console.error(' The build below probably failed.\n')
+        console.error('******************* READ ME ****************\n')
+      } else {
+        console.log('Using pre-bundled vcpkg')
+      }
+    }
+  } else if (process.platform == 'darwin') {
+    if (!fs.existsSync(`/usr/local/opt/zlib/include/`)) {
+      console.error('******************* READ ME ****************\n')
+      console.error(' zlib was not found. Run `xcode-select --install` and try again.\n')
+      console.error(' The build below probably failed.\n')
+      console.error('******************* READ ME ****************\n')
+    }
+  } else {
+    if (!fs.existsSync(`/usr/include/zlib.h`) && !fs.existsSync(`/usr/local/include/zlib.h`)) {
+      console.error('******************* READ ME ****************\n')
+      console.error(' zlib headers were not found. If the build fails, try `sudo apt-get install libz-dev`\n')
+      console.error('******************* READ ME ****************\n')
+    }
   }
 }
 
-console.log('Build checks are passing!')
+runChecks().then(
+  () => console.log('Build checks are passing!')
+)
+
+
 
 module.exports = () => {}
