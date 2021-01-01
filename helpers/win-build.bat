214768b9e9662945f8dfddcf413eb27abe412dff
extremeheat <extreme@protonmail.ch>
2020-12-31 20:30:33 -0500

    update

--- helpers/win-build.bat
+++ helpers/win-build.bat
@@ -1,5 +1,5 @@
 tar -xf win-vcpkg-export.tar.gz
 set CMAKE_TOOLCHAIN_FILE=%CD%\vcpkg-export\scripts\buildsystems\vcpkg.cmake
 echo '::set-output name=CMAKE_TOOLCHAIN_FILE::%CMAKE_TOOLCHAIN_FILE%'
-
+setx CMAKE_TOOLCHAIN_FILE %CMAKE_TOOLCHAIN_FILE%
 ::TODO: we should expand this to macOS also
