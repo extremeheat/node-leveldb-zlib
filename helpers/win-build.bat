5696307982bbdb4a6513da54de4e4dc3cbfd4bc6
extremeheat <extreme@protonmail.ch>
2020-12-31 19:55:12 -0500

    win helper script fix

--- helpers/win-build.bat
+++ helpers/win-build.bat
@@ -1,4 +1,4 @@
-tar -xz vcpkg-export.zip
+tar -xf win-vcpkg-export.tar.gz
 setx CMAKE_TOOLCHAIN_FILE %CD%\vcpkg-export\scripts\buildsystems\vcpkg.cmake
 
 ::TODO: we should expand this to macOS also
