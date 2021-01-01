6bc35552dcf2c363256fc97d757e9095884bd0e9
extremeheat <extreme@protonmail.ch>
2021-01-01 06:11:37 -0500

    updates

--- binding.js
+++ binding.js
@@ -1,5 +1,14 @@
+const fs = require('fs')
+const helper = require('./helpers/buildPath.js')
 var SegfaultHandler = require('segfault-handler')
 SegfaultHandler.registerHandler("crash.log")
-var bindings = require('bindings')('node-leveldb.node')
+
+var bindings
+var pathToSearch = helper.getPath()
+if (pathToSearch) {
+  bindings = require(pathToSearch + '/node-leveldb.node')
+} else {
+  bindings = require('bindings')('node-leveldb.node')
+}
 
 module.exports = bindings
---
+++ helpers/buildPath.js
@@ -0,0 +1,30 @@
+const fs = require('fs')
+const os = require('os')
+
+module.exports = {
+  getPath() {
+    let _osVersion = os.release()
+
+    let plat = process.platform
+    let arch = process.arch
+    let ver = _osVersion.split('.', 1)
+
+    let path = `./prebuilds/${plat}-${ver}-${arch}/`
+    if (fs.existsSync(path)) {
+      console.log('[leveldb] using prebuild in ', path)
+      return path
+    } else {
+      console.log('[leveldb] building as prebuild not found in ', path)
+      return
+    }
+  },
+  
+  getPlatformString() {
+    let _osVersion = os.release()
+
+    let plat = process.platform
+    let arch = process.arch
+    let ver = _osVersion.split('.', 1)
+    return `${plat}-${ver}-${arch}`
+  }
+}
